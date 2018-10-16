3038a7148cee13bb6a868102cec9260b86c741d2
Mo <42975535+MoIbrahim21@users.noreply.github.com>
2018-10-16 11:44:57 -0500

    Add files via upload

---
+++ src/Madlibsproject.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Madlibsproject {
+
+	public static void main(String[] args) 
+	{
+		Scanner verb = new Scanner(System.in);
+		System.out.print("Please enter a noun:   ");		
+		String noun = verb.nextLine();
+		System.out.print("\n Please enter a verb:  ");
+		String v1 = verb.nextLine();
+		System.out.print("\n Please enter an adjective:   ");
+		String adj1 = verb.nextLine();
+		System.out.print("\n Please enter a location:  ");
+		String location = verb.nextLine();
+		System.out.print("\n Please enter a time:  ");
+		String time = verb.nextLine();
+		System.out.println("\n please enter a noun:  ");
+		String noun2 = verb.nextLine();
+		System.out.print("\n Please enter a disgusting verb:  ");
+		String v2 = verb.nextLine();
+		System.out.print("\n Please enter a noun:  ");
+		String noun3 = verb.nextLine();
+		System.out.print("\n Please enter an adjective:  ");
+		String adj2 = verb.nextLine();
+		System.out.print("\n Please enter a weird verb:  ");
+		String v3 = verb.nextLine();
+		System.out.print("\n Please enter a noun:  ");
+		String noun4 = verb.nextLine();
+		System.out.print("\n Please enter a place:  ");
+		String location2 = verb.nextLine();
+		System.out.println("There once was a Noun who was funny and liked to V1 in a Adj1 way. This ussually took place at Location during a time with noun2 who also v2.");
+		
+	}
+
+}
